e8d3902f47f62a43f95f4a0ad5b0f189ffaf4ed6
Nicolas Moran <nicolasdavian@gmail.com>
2026-09-23 16:10:54 -0500

    Primera version: escaner de documentos con OCR en el celular, genera tabla de extranjeros identificados como imagen

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'escaner-migratorio-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  var url = event.request.url;
+  // el propio scanner (mismo origen) se sirve desde cache-first; librerias externas (tesseract/CDN) van a la red normal
+  if (url.indexOf(self.location.origin) === 0) {
+    event.respondWith(
+      caches.match(event.request).then(function(cached){
+        return cached || fetch(event.request);
+      })
+    );
+  }
+});
